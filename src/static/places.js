cdc246d210b663ed612942502829233c6d880894
ShwethaS04 <sa_shwetha@hotmail.com>
2021-05-01 16:13:57 +0530

    MaterialUI Layout Sample

---
+++ src/static/places.js
@@ -0,0 +1,19 @@
+const places = [
+    {
+        title: 'Green',
+        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+        imageUrl: process.env.PUBLIC_URL + '/assets/island1.jpg',
+        direction: 'left',
+        time: 1500,
+},
+{
+    title: 'Bora Bora',
+    description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.',
+    imageUrl: process.env.PUBLIC_URL + '/assets/island2.jpg',
+    direction: 'right',
+    time: 1500,
+},
+
+]
+
+export default places;
